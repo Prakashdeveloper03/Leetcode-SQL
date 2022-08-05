44fcce63feab41bd63abc9ca634e161a3399c5a6
Siva Prakash <prakashdeveloper@outlook.in>
2022-08-05 07:32:36 +0530

    Added Medium\Capital Gain Loss folder

---
+++ Medium/Capital Gain Loss/query.sql
@@ -0,0 +1,12 @@
+SELECT
+    stock_name,
+    SUM(
+        CASE
+            WHEN operation = 'Buy' THEN - price
+            ELSE price
+        END
+    ) AS capital_gain_loss
+FROM
+    stocks
+GROUP BY
+    stock_name;
